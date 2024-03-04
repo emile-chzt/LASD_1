d4f68805af03ae829323cd7508e2eef1e97ad7e0
Faustine Lefebvre <lefebvrefaustine1@gmail.com>
2024-03-04 21:35:33 +0100

    applicaciones_web_gestion/nodes_module/

---
+++ applicacion_web_gestion/app/routes/api.js
@@ -0,0 +1,14 @@
+var bodyParser = require('body-parser');  // get body-parser
+var jwt        = require('jsonwebtoken'); // para trabajar el token
+var fs         = require('fs');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
+module.exports = function(app, express) {
+
+  var apiRouter = express.Router();
+
+
+	return apiRouter;
+  
+};
